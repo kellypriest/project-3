a10c84b6df602be884462554e5b1da3ce0e38b73
angiekl74 <angiekl74@hotmail.com>
2020-05-27 21:45:42 -0700

    Pushing FINAL CLEANED Louisville csv and notebook

--- app.py
+++ app.py
@@ -4,24 +4,26 @@ import pickle
 
 
 app = Flask(__name__)
-# model = pickle.load(open('model.pkl', 'rb'))
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/predict',methods=['POST'])
-def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = rf_model.predict(final_features)
-
-    output = prediction[0]
-
-    return render_template('index.html', prediction_text={output})
+# prediction function 
+def ValuePredictor(to_predict_list): 
+    to_predict = np.array(to_predict_list).reshape(1, 12) 
+    loaded_model = pickle.load(open("model.pkl", "rb")) 
+    result = loaded_model.predict(to_predict) 
+    return result[0] 
+  
+@app.route('/predict', methods = ['POST']) 
+def predict(): 
+    if request.method == 'POST': 
+        to_predict_list = request.form.to_dict() 
+        to_predict_list = list(to_predict_list.values()) 
+        to_predict_list = list(map(int, to_predict_list)) 
+        result = ValuePredictor(to_predict_list)         
+        if int(result)== 1: 
+            prediction ='Take'
+        else: 
+            prediction ='Deny'            
+        return render_template("templates/index.html", prediction = prediction)
 
 if __name__ == "__main__":
     app.run(debug=True)
