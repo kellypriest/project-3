179c58aa3914beeb1a57afeb61e309ce30317cbb
angiekl74 <angiekl74@hotmail.com>
2020-05-28 12:50:32 -0700

    Working Flask app with RF model

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
-import numpy as np
+from flask import Flask, render_template, request
 from flask import Flask, request, jsonify, render_template
 import pickle
-
+import numpy as np
 
 app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
 
 # prediction function 
+@app.route("/")
+def home():
+    return render_template ("index.html")
+
 def ValuePredictor(to_predict_list): 
-    to_predict = np.array(to_predict_list).reshape(1, 12) 
+    to_predict = np.array(to_predict_list).reshape(1, 6) 
     loaded_model = pickle.load(open("model.pkl", "rb")) 
     result = loaded_model.predict(to_predict) 
     return result[0] 
   
-@app.route('/predict', methods = ['POST']) 
-def predict(): 
+@app.route('/result', methods = ['POST']) 
+def result(): 
     if request.method == 'POST': 
         to_predict_list = request.form.to_dict() 
         to_predict_list = list(to_predict_list.values()) 
@@ -22,8 +28,8 @@ def predict():
         if int(result)== 1: 
             prediction ='Take'
         else: 
-            prediction ='Deny'            
-        return render_template("templates/index.html", prediction = prediction)
+            prediction ='No'            
+        return render_template("result.html", prediction = prediction) 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+   app.run(debug = True)
