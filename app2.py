c6780ce53883b43ba434f369ae8c8b3b9e2bdb8b
angiekl74 <angiekl74@hotmail.com>
2020-05-30 21:39:32 -0700

    Create pkl file modelonehot, NN model hypertuning

--- app2.py
+++ app2.py
@@ -14,8 +14,8 @@ def home():
     return render_template ("index2.html")
 
 def ValuePredictor(to_predict_list): 
-    to_predict = np.array(to_predict_list)   
-    loaded_model = pickle.load(open("rf_modelTESTMarc.pkl", "rb")) 
+    to_predict = np.array(to_predict_list).reshape(1, 52)     
+    loaded_model = pickle.load(open("modelonehot.pkl", "rb")) 
     result = loaded_model.predict(to_predict) 
     return result[0]
 
@@ -24,19 +24,29 @@ def result():
     if request.method == 'POST': 
         to_predict_list = request.form.to_dict()
         to_predict_list = list(to_predict_list.values()) 
-        # new_list=list(map(lambda x: float(x.replace(",", "")), to_predict_list))
-        # to_predict_list = [(re.sub("\'", '', str(to_predict_list)))]
-        # to_predict_list = (re.sub("\'", '', str(to_predict_list)))
-        # to_predict_list = [[0, 0, 0, 1, 1, 0]]
-                            
-        # loaded_model = pickle.load(open("rf_modelTESTMarc.pkl", "rb")) 
-        # result = loaded_model.predict(to_predict_list)
-               
-        # if result == "TAKE": 
-        #     outcome ='Take'
-        # else: 
-        #     outcome ='Deny'            
-        return render_template("result2.html", outcome = to_predict_list) 
+        new_list=list(map(lambda x: int(x.replace(",", "")), to_predict_list))
+
+        zero_list = []
+        for x in range(52):
+            zero_list.append(0)
+        for x in new_list:
+            zero_list[x] = 1
+
+        result = ValuePredictor(zero_list)       
+        if result == 1: 
+            outcome ='Take'
+        else: 
+            outcome ='Deny'            
+        return render_template("result2.html", outcome = outcome) 
 
 if __name__ == '__main__':
    app.run(debug = True)
+
+# zero_list = []
+# for x in range(52):
+#     zero_list.append(0)
+# print(zero_list)
+# user_inputs = [1, 24, 37, 39, 44, 51]
+# for x in user_inputs:
+#     zero_list[x] = 1
+# print(zero_list)
